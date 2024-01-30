0a7142cc27633ce6f0a916cc3ba224e92a11ce0c
Ivan Belyaev <150545682+Uzumko13@users.noreply.github.com>
2024-01-30 23:23:22 +0300

    add FeedViewController

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -1,29 +1,56 @@
-//
-//  FeedViewController.swift
-//  Navigation
-//
-//  Created by Иван Беляев on 30.01.2024.
-//
-
 import UIKit
 
 class FeedViewController: UIViewController {
-
+    
+    private lazy var actionButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 12
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Переход на пост", for: .normal)
+        button.setTitleColor(.systemOrange, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        
+        
+        return button
+    }()
+    
+    var post = Post(title: "Мои новости")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
+        
+        view.addSubview(actionButton)
+        
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            actionButton.leadingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.leadingAnchor,
+                constant: 20.0
+            ),
+            actionButton.trailingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
+            actionButton.centerYAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.centerYAnchor),
+            actionButton.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+        
+        actionButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func buttonPressed(_ sender: UIButton) {
+        
+        let postViewController = PostViewController()
+        
+        postViewController.titlePost = post.title
+        
+        self.navigationController?.pushViewController(postViewController, animated: true)
+        
+        self.navigationItem.title = postViewController.titlePost
     }
-    */
-
 }
