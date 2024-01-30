32a4c348bf1b639be67610ab81b9351a2bdddd28
Ivan Belyaev <150545682+Uzumko13@users.noreply.github.com>
2024-01-30 23:26:46 +0300

    add PostViewController

---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+class PostViewController: UIViewController {
+    
+    private lazy var actionButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .green
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Новость", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        
+        return button
+    }()
+    
+    var titlePost = "News Post"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBrown
+        
+        view.addSubview(actionButton)
+        
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            actionButton.leadingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.leadingAnchor,
+                constant: 20.0
+            ),
+            actionButton.trailingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
+            actionButton.centerYAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.centerYAnchor),
+            actionButton.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+        
+        actionButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        
+    }
+    
+    @objc func buttonPressed(_ sender: UIButton) {
+        
+        let infoViewController = InfoViewController()
+        
+        infoViewController.modalTransitionStyle = .coverVertical
+        infoViewController.modalPresentationStyle = .pageSheet
+        
+        present(infoViewController, animated: true)
+        
+    }
+}
