cee1b30e97011979a97902fe0b5ad57c459666d8
Ivan Belyaev <150545682+Uzumko13@users.noreply.github.com>
2024-01-31 09:09:46 +0300

    change commit FeedViewController.swift

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -20,7 +20,7 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .Gray
+        view.backgroundColor = .gray
         
         view.addSubview(actionButton)
         
