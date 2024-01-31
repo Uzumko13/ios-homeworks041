21782fd5071cfd68c8c72161d9b3513eaaf9c10b
Ivan Belyaev <150545682+Uzumko13@users.noreply.github.com>
2024-01-31 09:11:16 +0300

    change ProfileHeaderView

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -1,20 +1,4 @@
-//
-//  ProfileHeaderView.swift
-//  Navigation
-//
-//  Created by Иван Беляев on 31.01.2024.
-//
-
 import UIKit
 
 class ProfileHeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
