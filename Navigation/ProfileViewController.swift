cee52803273175cf6b699e5a290c6b67ffb6a6a9
Ivan Belyaev <150545682+Uzumko13@users.noreply.github.com>
2024-01-31 09:05:21 +0300

    change ProfileViewController

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -5,7 +5,7 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .gray
+        view.backgroundColor = .lightGray
         
     }
 }
