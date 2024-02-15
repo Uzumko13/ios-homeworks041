847c0c270e20a2c95860a87a1cfcf6cb9465e07b
Ivan Belyaev <150545682+Uzumko13@users.noreply.github.com>
2024-02-15 13:33:46 +0300

    final commit HW2.2 modifed ProfileHeaderView

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -79,14 +79,8 @@ final class ProfileHeaderView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(fullNameLabel)
-        addSubview(avatar)
-        addSubview(statusLabel)
-        addSubview(statusTextField)
-        addSubview(statusButton)
-    
         setupConstraint()
-        
+        setupAddSubview()
         statusTextField.delegate = self
     }
     
@@ -96,31 +90,39 @@ final class ProfileHeaderView: UIView {
     
     //MARK: Setup metohd
     
+    private func setupAddSubview() {
+        addSubview(fullNameLabel)
+        addSubview(avatar)
+        addSubview(statusLabel)
+        addSubview(statusTextField)
+        addSubview(statusButton)
+    }
+    
     private func setupConstraint() {
         NSLayoutConstraint.activate([
             fullNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
             fullNameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 156),
-            fullNameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            fullNameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: LayoutConstants.trailingMargin),
             fullNameLabel.heightAnchor.constraint(equalToConstant: 28),
             
-            avatar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
-            avatar.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            avatar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: LayoutConstants.indent),
+            avatar.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: LayoutConstants.leadingMargin),
             avatar.widthAnchor.constraint(equalToConstant: 128),
             avatar.heightAnchor.constraint(equalTo: avatar.widthAnchor),
             
-            statusLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 16),
+            statusLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: LayoutConstants.indent),
             statusLabel.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
             statusLabel.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor),
             statusLabel.heightAnchor.constraint(equalTo: fullNameLabel.heightAnchor),
             
-            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 16),
+            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: LayoutConstants.indent),
             statusTextField.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
             statusTextField.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor),
             statusTextField.heightAnchor.constraint(equalToConstant: 40),
             
-            statusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: 16),
-            statusButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            statusButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            statusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: LayoutConstants.indent),
+            statusButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: LayoutConstants.leadingMargin),
+            statusButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: LayoutConstants.trailingMargin),
             statusButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
